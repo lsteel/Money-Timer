96324b25e7d923ef6fe96f382d64ca392164b8ed
lsteel <lorinsteel@gmail.com>
2016-09-11 23:07:40 -0600

    Configured webpack for babel. Bootstrapped react boilerplate. Set up components organization in client folder.

--- routes/time-punches.js
+++ routes/time-punches.js
@@ -11,4 +11,19 @@ timePunchesRouter.get('/', (req, res, next) => {
   res.json(timePunches)
 })
 
+timePunchesRouter.post('/', (req, res, next) => {
+  if (!req.body.startTime) {
+    return next(new Error('You must include a `name` in the body'))
+  }
+  if (typeof req.body.startTime !== 'string') {
+    return next(new Error('The `name` property must be a string'))
+  }
+
+  let newTimePunchId = timePunches.push({
+    startTime: req.body.startTime,
+  })
+
+  res.json(timePunches[newTimePunchId])
+})
+
 export default timePunchesRouter
